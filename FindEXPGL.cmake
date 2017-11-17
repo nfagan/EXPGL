6c943427040a383d82667eb585727f8ae2ec72aa
nfagan <nick@fagan.org>
2017-11-16 23:33:10 -0500

    update build file for osx

--- FindEXPGL.cmake
+++ FindEXPGL.cmake
@@ -1,3 +1,12 @@
 set(EXPGL_INCLUDE_DIR "${EXP_ROOT_DIR}/EXPGL/include")
 set(EXPGL_LIBRARY_DIR "${EXP_ROOT_DIR}/EXPGL/lib")
+
 set(EXPGL_LIBRARIES "EXPGL" "glfw3")
+
+if (APPLE)
+	list(APPEND EXPGL_LIBRARIES 
+		"-framework OpenGL"
+		"-framework CoreVideo" 
+		"-framework IOKit" 
+		"-framework Cocoa")
+endif()
